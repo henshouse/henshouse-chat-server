ff7ad99f16608c6fdbf28400ef8a234481679f74
filip2cz <filda.komarek@seznam.cz>
2021-12-20 08:42:08 +0100

    old version for re-release 1.6 in new repository

--- server.py
+++ server.py
@@ -1,22 +1,86 @@
-# region imports
+import socket as sc
 from threading import Thread
+from datetime import datetime
 from time import sleep
 import os
 import signal
-import sys
-import asyncio
 from typing import Union
-import websockets as ws
+import argparse
+import sys
 
-from constants import NAME_SPLITTER, VERSION, PORT
-from connection import Connection
-from security import Asymmetric
-from log import log_start, log
-# endregion
+from constants import *
+from security import *
+
+
+class Connection(Thread):
+    @staticmethod
+    def makenew(sock, addr, server, n):
+        conn = Connection(sock, addr, server, n)
+        conn.start()
+        return conn
+
+    def __init__(self, sock, addr, server, n):
+        Thread.__init__(self)
+        self.sock = sock
+        self.addr = addr
+
+        self.server = server
+
+        self.msgs = []
+
+        self.nick = str(n)
+
+        self.send(self.server.pbl_key.export_key())
+        self.c_key = import_key(self.recv())
+        self.send_encrypted(self.nick)
+
+    def send(self, msg: bytes):
+        self.sock.send(bytes(str(len(msg)).zfill(
+            MSG_ZFILL), encoding=ENCODING) + msg)
+
+    def send_encrypted(self, msg: str):
+        msg_bytes = encrypt(msg, self.c_key)
+        self.send(msg_bytes)
+
+    def recv(self):
+        n_bytes = int(self.sock.recv(MSG_ZFILL))
+        msg_bytes = self.sock.recv(n_bytes)
+        return msg_bytes
+
+    def recv_decrypted(self):
+        msg_bytes = self.recv()
+        return decrypt(msg_bytes, self.server.prv_key)
+
+    def run(self):
+        def close():
+            self.server.conns.remove(self)
+            self.sock.close()
+
+        try:
+            while self.server.running:
+                msg = self.recv_decrypted()
+                print(f'[*][{datetime.now()}]  ' + self.nick + ' > ' + msg)
+                _msg = msg.replace('  ', ' ')
+                while '  ' in _msg:
+                    _msg = _msg.replace('  ', ' ')
+                if _msg.split(' ')[0].lower() == '/nick' and len(_msg.split(' ')) > 1:
+                    self.nick = _msg.split(' ')[1]
+                self.server.send_to_all(msg, self)
+                log_message(self, msg)
+        except ConnectionResetError:
+            print(f'[!][{datetime.now()}] Connection error, closing connection')
+            close()
+        except TimeoutError:
+            print(f'[!][{datetime.now()}] Timeout error, closing connection')
+            close()
+        except Exception as e:
+            print(
+                f'[!][{datetime.now()}] Error occured: {e}, closing connection')
+            close()
 
 
 class Server:
-    def __init__(self, ip: str, port: int, max_conn: int = -1):
+    def __init__(self, ip: str, port: int, max_conn: -1):
         self.ip = ip
         self.port = port
         self.addr = (self.ip, self.port)
@@ -25,10 +89,10 @@ class Server:
         self.running = True
         self.conns = []
 
-        self.local_asym = Asymmetric.new()
-
+        self.prv_key, self.pbl_key = generate_keys()
+        self.sock = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
         log_start()
-        asyncio.run(self.run_server())
+        self.run_server()
 
     def start_exit_thread(self):
         def quit_thread():
@@ -39,80 +103,72 @@ class Server:
                         os.kill(os.getpid(), signal.SIGINT)
                     elif cmd == 'help':
                         print(
-                            f'> enter any of "q", "quit", ":q", ":q!" or "exit" and press enter to quit')
+                            f'> enter any of "q", "quit", ":q", ":q!", "exit" and press enter to quit')
                     else:
                         print(f'[!] Command unknown')
             except EOFError:
                 os.kill(os.getpid(), signal.SIGINT)
         Thread(target=quit_thread).start()
 
-    async def run_server(self):
-        n = 1
+    def run_server(self):
+        try:
+            self.sock.bind(self.addr)
+            self.sock.listen(self.max_conn)
+        except OSError as e:
+            print(f'[!][{datetime.now()}] Error binding, you propably are propably trying to run multiple servers on same port at the same time')
+            return
 
-        async def new_conn(wsock: ws.WebSocketServerProtocol):
-            nonlocal n
-            nick = str(n)
-            n += 1
-            conn = Connection(wsock, wsock.remote_address[0], self, nick)
-            self.conns.append(conn)
-            await conn.run()
+        print(f'[*][{datetime.now()}] Server Running')
+        print(f'[*][{datetime.now()}] Listening on port {self.port}')
 
         self.start_exit_thread()
 
-        log(f'Server Running')
-        log(f'Listening on port {self.port}')
+        n = 1
 
-        try:
-            async with ws.serve(new_conn, self.ip, self.port):
-                await asyncio.Future()
-        except KeyboardInterrupt:
-            raise KeyboardInterrupt
-        except Exception as e:
-            log(e)
+        while self.running:
+            (client_sock, addr) = self.sock.accept()
+            ct = Connection.makenew(client_sock, addr, self, n)
+            self.conns.append(ct)
+            print(f'[*][{datetime.now()}] User {addr} ({n}) logged in')
+            log_login(addr, str(n))
+            self.send_to_all(f'User {n} logged in', str(n))
+            n += 1
 
-    async def send_to_all(self, msg, author: Union[Connection, str]):
+    def send_to_all(self, msg, author: Union[Connection, str]):
         msg = (author.nick if isinstance(author, Connection)
                else author) + NAME_SPLITTER + msg
-        to_remove = []
         for conn in self.conns:
-            msg_with_me = conn.nick + NAME_SPLITTER + msg
-            try:
-                await conn.send_str_sym(msg_with_me)
-            except ws.exceptions.ConnectionClosedOK:
-                to_remove.append(conn)
-        for conn in to_remove:
-            if conn in self.conns:
-                self.conns.remove(conn)
-            await conn.close()
+            # conn = Connection()
+            conn.send_encrypted(msg)
 
-    async def send_to_all_raw(self, msg, author: Union[Connection, str]):
-        msg = (author.nick if isinstance(author, Connection)
-               else author) + NAME_SPLITTER + msg
-        to_remove = []
-        for conn in self.conns:
-            msg_with_me = conn.nick + NAME_SPLITTER + msg
-            try:
-                await conn.send(msg_with_me)
-            except ws.exceptions.ConnectionClosedOK:
-                to_remove.append(conn)
-        for conn in to_remove:
-            if conn in self.conns:
-                self.conns.remove(conn)
-            await conn.close()
+
+def log_message(conn: Connection, msg):
+    with open('msgs.log', 'a', encoding='utf-8') as f:
+        f.writelines(
+            [f'[{datetime.now()}] {conn.nick} {conn.addr}: {msg}', '\n'])
+
+
+def log_start():
+    with open('msgs.log', 'a') as f:
+        f.write(f'[{datetime.now()}] Server started\n')
+
+
+def log_login(addr, nick):
+    with open('msgs.log', 'a') as f:
+        f.write(f'[{datetime.now()}] User {addr} ({nick}) logged in\n')
 
 
 def get_ip():
-    import socket as sc
     sock = sc.socket(sc.AF_INET, sc.SOCK_DGRAM)
     sock.connect(("8.8.8.8", 80))
+    # print("[*] Write this IP to other computers: " + sock.getsockname()[0])
     server = sock.getsockname()[0]
     sock.close()
     sleep(0.5)
     return server
 
 
-def run():
-    import argparse
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(f'--port', type=int,
                         help='port number for server, defaults to {PORT}')
@@ -127,28 +183,9 @@ def run():
     else:
         _port = PORT
     # _ip = '10.0.1.17'
-    log(f'You are running version {VERSION}')
+    print(f'[*][{datetime.now()}] You are running version {VERSION}')
 
     _ip = get_ip()
 
-    log(f"Running on {(_ip, _port)}")
-    Server(_ip, _port)
-
-
-def test_server():
-    import asyncio
-    import websockets
-
-    async def echo(websocket):
-        async for message in websocket:
-            await websocket.send(message)
-
-    async def main():
-        async with websockets.serve(echo, "localhost", 8765):
-            await asyncio.Future()  # run forever
-
-    asyncio.run(main())
-
-
-if __name__ == '__main__':
-    run()
+    print(f"[*][{datetime.now()}] Running on {(_ip, _port)}")
+    Server(_ip, _port, 5)
